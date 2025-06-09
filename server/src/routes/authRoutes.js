da899d5d6d72a0dfe54848506d6fab557913eb83
christopherrca <rocharocharocha1995@yahoo.com>
2025-06-09 19:03:45 -0400

    created authCONTROLLERS

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,30 +1,10 @@
 import { Router } from "express"
 import { User } from "../models/userModels.js";
+import { authCallback } from "../controllers/authControllers.js";
 
 const router = Router()
 
-router.post("/callback", async(req, res) => {
-    try {
-        const {id, firstName, lastName, imageUrl} = req.body;
-
-
-        // check if user already exists
-        const user = await User.findOne({clerkId: id})
-
-        if(!user){
-            // signup
-            await User.create({
-                clerkId: id,
-                fullName: `${firstName} ${lastName}`,
-                imageUrl
-            })
-        }
-        res.status(200).json({success: true})
- 
-    } catch(error) {
-
-    }
-})
+router.post("/callback", authCallback)
 
 
 
